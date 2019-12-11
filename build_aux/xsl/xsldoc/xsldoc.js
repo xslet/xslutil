fc3fd086157a0a443f591ba3cde5eef99054f2d1
sttk <sttk.xslet@gmail.com>
2019-12-12 00:48:36 +0900

    Update: Update xsldoc.

--- build_aux/xsl/xsldoc/xsldoc.js
+++ build_aux/xsl/xsldoc/xsldoc.js
@@ -5,6 +5,9 @@ function printPageTitle() {
   var fpath = location.href;
   var index = fpath.lastIndexOf('/');
   var fname = (index < 0) ? fpath : fpath.substring(index + 1);
+  if (fname.slice(-4) === '.xml') {
+    fname = fname.slice(0, -4) + '.xsl';
+  }
   var title = fname + ' - API Document';
   document.querySelector('h1').textContent = title;
   document.title = title;
